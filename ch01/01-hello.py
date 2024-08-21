8e573c63837d131ee5a352c0d8ab7815ed6908d4
Mr.Moon <MrMoonKr@gmail.com>
2024-08-21 14:13:55 +0900

    파이썬 소스 코드 디버그 모드 실행 설정 추가 ...

---
+++ ch01/01-hello.py
@@ -0,0 +1,7 @@
+print( 'Hello, Python : 안녕 파이썬 !' )
+print( 'Hello, Python : 안녕 파이썬 !!' )
+print( 'Hello, Python : 안녕 파이썬 !!!' )
+print( 'Hello, Python : 안녕 파이썬 !!!!' )
+print( 'Hello, Python : 안녕 파이썬 !!!!!' )
+print( 'Hello, Python : 안녕 파이썬 !!!!!!' )
+print( 'Hello, Python : 안녕 파이썬 !!!!!!!' )
